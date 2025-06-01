4253146eafc2334968c0147aaa84f8e913e61430
Vinay Jadhav <jadhavvinay163@gmail.com>
2025-06-01 17:56:09 +0530

    Updatedd

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,7 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export const USER_API_END_POINT = `${BACKEND_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${BACKEND_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${BACKEND_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${BACKEND_URL}/api/v1/company`;
-export const SAVE_JOB_API_END_POINT = `${BACKEND_URL}/api/v1/job/save`;
-
+export const USER_API_END_POINT = `${BACKEND_URL}/api/v1/users`;
+export const JOB_API_END_POINT = `${BACKEND_URL}/api/v1/jobs`; // fixed
+export const APPLICATION_API_END_POINT = `${BACKEND_URL}/api/v1/applications`; // fixed
+export const COMPANY_API_END_POINT = `${BACKEND_URL}/api/v1/companies`; // fixed
+export const SAVE_JOB_API_END_POINT = `${BACKEND_URL}/api/v1/savedjobs`; // also fixed if you're using `savedJobRoute`
