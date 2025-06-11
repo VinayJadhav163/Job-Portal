ac4f70559fe3a1e7d58b7b278204ff02b1e8f366
Vinay Jadhav <jadhavvinay163@gmail.com>
2025-06-11 09:45:39 +0530

    ONE Place

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,16 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensures built files go here
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
